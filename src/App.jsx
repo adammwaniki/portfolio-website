5eae1d6a43b0fa6e9b20b1172bcbe695387e4355
Adam Mwaniki <adamndegwa@protonmail.com>
2024-09-30 02:55:40 +0300

    experiment 2 with routes

--- src/App.jsx
+++ src/App.jsx
@@ -6,30 +6,19 @@ import Navbar from "./components/Navbar"
 import DateTimeDisplay from "./components/DateTimeDisplay"
 import { Routes, Route, Navigate } from "react-router-dom"
 
-
 export default function App() {
-  
-
   return (
-    <>
-      <div className="bg-[#FFFFF0] w-lvw">
-        <Routes>
-          <Route path="/" element={<Navigate to="/home" />} />
-          <Route path="/home" element={
-            <>
-            <DateTimeDisplay/>,
-            <HomePage/>,
-            <About/>,
-            <Projects/>,
-            <Blog/>,
-            <Navbar/>
-            </>} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/blog" element={<Blog />} />
-        </Routes>
-        
-      </div>
-    </>
+    <div className="bg-[#FFFFF0] w-lvw">
+      <Navbar />
+      <DateTimeDisplay />
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="*" element={<Navigate to="/home" />} />
+      </Routes>
+    </div>
   )
 }
