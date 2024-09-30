4c801611ca0c5df984ee423370a6dbe545b7adf2
Adam Mwaniki <adamndegwa@protonmail.com>
2024-09-30 17:52:37 +0300

    Adds first blog

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -7,12 +7,12 @@ export default function Blog() {
             <div className='flex justify-center min-h-screen'>
                 <div className='flex flex-wrap justify-center  p-1 mt-[16vh]'>
                     <BlogCard
-                        imgSrc='https://picsum.photos/id/10/300/200'
-                        imgAlt='This Website'
+                        imgSrc='https://miro.medium.com/v2/resize:fit:1400/format:webp/1*ix7zmK2zTyCTKiWQQR_Gdg.png'
+                        imgAlt='A Blog About This Portfolio Website'
                         title="Mwaniki's Website"
                         description='Join me on my journey of building this website from scratch! Discover how I harnessed the power of React JS and Tailwind CSS to create a stunning, user-friendly experience that marries sleek design with dynamic functionality. '
                         buttonText='View Blog'
-                        link='https://github.com/adammwaniki/SendIT'
+                        link='https://medium.com/@adamndegwa/my-journey-to-building-a-portfolio-website-from-scratch-2559c7211528'
                     />
                 
                     <BlogCard
