46e7fd23ada5c5fb9d23d6efbdba7f7d3ec3a90e
Adam Mwaniki <adamndegwa@protonmail.com>
2024-09-30 03:10:04 +0300

    Makes changes to package.json for gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,22 @@ import Projects from "./components/Projects"
 import Blog from "./components/Blog"
 import Navbar from "./components/Navbar"
 import DateTimeDisplay from "./components/DateTimeDisplay"
-import { Routes, Route, Navigate } from "react-router-dom"
+//import { Routes, Route, Navigate } from "react-router-dom"
+
 
 export default function App() {
+  
+
   return (
-    <div className="bg-[#FFFFF0] w-lvw">
-      <Navbar />
-      <DateTimeDisplay />
-      <Routes>
-        <Route path="/" element={<Navigate to="/home" />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="*" element={<Navigate to="/home" />} />
-      </Routes>
-    </div>
+    <>
+      <div className="bg-[#FFFFF0] w-lvw">
+        <DateTimeDisplay/>
+        <HomePage/>
+        <About/>
+        <Projects/>
+        <Blog/>
+        <Navbar/>
+      </div>
+    </>
   )
 }
